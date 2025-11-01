128b4adab7cba53c55aaadee4542b7a35b32df00
Yakiboshi <shosei.sgalc@gmail.com>
2025-11-01 19:57:43 +0900

    Initial Commit

---
+++ Voice to do App/Voice_to_do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Voice_to_do_AppApp.swift
+//  Voice to do App
+//
+//  Created by 焼星　指紋 on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct Voice_to_do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
